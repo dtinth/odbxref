eb393bc62355375b3a136d6d6156027a9702d5ce
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2014-05-15 23:51:56 +0700

    update update script

--- update.sh
+++ update.sh
@@ -4,7 +4,8 @@ archive() {
   echo 'Adding to index:' $1-$2
   ruby odbxref.rb archive $1 $2
 }
-# archive `date -v-1m +'%Y %m'`
+archive `date -v-1m +'%Y %m'`
 archive `date +'%Y %m'`
 ruby odbxref.rb chapters
-# git commit -am "Update index for `date +'%Y-%m-%d'`"
+git add -A
+git commit -m "$(cat commit.msg)"
