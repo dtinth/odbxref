4a11286c2743c3a87024d9f1601cc3bede2e5287
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2014-05-19 08:25:55 +0700

    perform a git pull before automatic update

--- update_in_docker.sh
+++ update_in_docker.sh
@@ -3,5 +3,6 @@ set -e
 echo -n "Start: "
 date
 cd "`dirname "$0"`"
+git pull
 docker run --rm -v "$(pwd):/odbxref" odbxref bash -c 'cd odbxref && git config --global user.name "odbxref Bot" && git config --global user.email "odbxref@dt.in.th" && bash update.sh'
 git push
