0fcce824a4afc8fd3e15e8044a87de13f94bd17d
Thai Pangsakulyanont (@ tomoko) <org.yi.dttvb@gmail.com>
2014-05-19 08:15:41 +0700

    chdir to docker script

--- update_in_docker.sh
+++ update_in_docker.sh
@@ -1,4 +1,5 @@
 
 set -e
+cd "`dirname "$0"`"
 docker run --rm -v "$(pwd):/odbxref" odbxref bash -c 'cd odbxref && git config --global user.name "odbxref Bot" && git config --global user.email "odbxref@dt.in.th" && bash update.sh'
 git push
